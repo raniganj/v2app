581803b801d79ffc1f9bee2d1cdc841cd377f723
sunilmondal4you <sunilmondal4you@gmail.com>
2023-07-27 11:00:14 +0530

    home, about page

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,22 +1,27 @@
 <template>
-  <h1>Home Page</h1>
-  <div class="fs-4">
-    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ad sunt ipsum rem voluptate magni
-    totam tempora suscipit sapiente, aliquam atque voluptas eligendi voluptatem soluta est
-    blanditiis temporibus quibusdam explicabo. Consequatur aliquid a voluptatem. Veniam eligendi
-    corrupti obcaecati, sed inventore facilis dignissimos reprehenderit id mollitia quam in
-    accusantium asperiores aliquam? Ducimus, quas omnis. Inventore quia ad cupiditate magni pariatur
-    fugit repellendus rerum reiciendis sapiente, quibusdam minus quo rem eos nulla? Provident,
-    corporis. Impedit, ducimus quas molestias sunt soluta, voluptatum perspiciatis accusantium
-    maxime aliquid quisquam quos quasi quo temporibus porro cupiditate nulla veritatis, voluptates
-    commodi fugit. Iusto neque recusandae aperiam. Deleniti corporis, reprehenderit nihil quisquam
-    qui laudantium labore eligendi at unde praesentium voluptate nemo, facere, accusamus deserunt.
-    Tenetur, sint, officia exercitationem minima aspernatur ullam earum porro odit odio libero unde
-    sequi aliquam molestias eligendi? Aliquid, minima? Magnam error nulla perspiciatis officia
-    facilis non omnis quam a aspernatur, reprehenderit esse, natus neque? Aspernatur quae odio
-    aperiam debitis excepturi possimus autem laborum accusamus, ducimus voluptatibus tempora alias
-    ipsam optio dolore voluptates nemo id error, quis repellendus incidunt magni ea? Nostrum ducimus
-    dicta eligendi exercitationem! Sunt, corrupti assumenda voluptas laboriosam eaque tempore
-    voluptatibus nam, molestiae quo ipsum at dolor ipsam, aperiam ab veritatis eos hic?
+  <div class="row justify-content-center">
+    <div class="col-12 col-md-6">
+      <h1>Home Page</h1>
+      <div class="fs-4">
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ad sunt ipsum rem voluptate magni
+        totam tempora suscipit sapiente, aliquam atque voluptas eligendi voluptatem soluta est
+        blanditiis temporibus quibusdam explicabo. Consequatur aliquid a voluptatem. Veniam eligendi
+        corrupti obcaecati, sed inventore facilis dignissimos reprehenderit id mollitia quam in
+        accusantium asperiores aliquam? Ducimus, quas omnis. Inventore quia ad cupiditate magni
+        pariatur fugit repellendus rerum reiciendis sapiente, quibusdam minus quo rem eos nulla?
+        Provident, corporis. Impedit, ducimus quas molestias sunt soluta, voluptatum perspiciatis
+        accusantium maxime aliquid quisquam quos quasi quo temporibus porro cupiditate nulla
+        veritatis, voluptates commodi fugit. Iusto neque recusandae aperiam. Deleniti corporis,
+        reprehenderit nihil quisquam qui laudantium labore eligendi at unde praesentium voluptate
+        nemo, facere, accusamus deserunt. Tenetur, sint, officia exercitationem minima aspernatur
+        ullam earum porro odit odio libero unde sequi aliquam molestias eligendi? Aliquid, minima?
+        Magnam error nulla perspiciatis officia facilis non omnis quam a aspernatur, reprehenderit
+        esse, natus neque? Aspernatur quae odio aperiam debitis excepturi possimus autem laborum
+        accusamus, ducimus voluptatibus tempora alias ipsam optio dolore voluptates nemo id error,
+        quis repellendus incidunt magni ea? Nostrum ducimus dicta eligendi exercitationem! Sunt,
+        corrupti assumenda voluptas laboriosam eaque tempore voluptatibus nam, molestiae quo ipsum
+        at dolor ipsam, aperiam ab veritatis eos hic?
+      </div>
+    </div>
   </div>
 </template>
