32ca084c6ecbbf0c2291bda658504a36d10d8c49
sunilmondal4you <sunilmondal4you@gmail.com>
2023-07-30 07:54:08 +0530

    axios API call

--- src/views/MyAxiosApi.vue
+++ src/views/MyAxiosApi.vue
@@ -1,5 +1,6 @@
 <script setup>
 import { ref } from 'vue'
+import axios from 'axios'
 
 let post = ref()
 
@@ -7,16 +8,29 @@ let fetchPost = async () => {
   let response = await fetch('https://jsonplaceholder.typicode.com/posts/1')
   post.value = await response.json()
 }
+
+let fetchPostAxios = async () => {
+  let url = 'https://jsonplaceholder.typicode.com/posts/2'
+  let response = await axios.get(url)
+  post.value = response.data
+}
 </script>
 <template>
   <div class="row justify-content-center">
     <div class="col-12 col-md-6">
       <div class="fs-3">My Axioz API call</div>
       <hr />
-      <div>
-        <button type="button" class="btn btn-primary" @click="fetchPost()">Fetch Post</button>
+      <div v-if="post">
+        <div>Id : {{ post?.id }}</div>
+        <div>User Id : {{ post?.userId }}</div>
+        <div>Title : {{ post?.title }}</div>
       </div>
-      <div>{{ post }}</div>
+      <div>
+        <button type="button" class="btn btn-primary me-1" @click="fetchPost()">Fetch Post</button>
+        <button type="button" class="btn btn-primary" @click="fetchPostAxios()">
+          Fetch Post with Axios
+        </button>
+      </div>
     </div>
   </div>
 </template>
